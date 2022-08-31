885af03f8fae6010dfc8f1195f122aa3bc2c5a58
Hickups789 <jackysantiago358@yahoo.com>
2022-08-31 18:39:02 -0400

    seeds

---
+++ db/seeds.sql
@@ -0,0 +1,28 @@
+INSERT INTO department (name)
+VALUES
+('Engineering'),
+('Finance'),
+('Legal'),
+('Sales');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Software Engineer', 120000, 1),
+('Lead Engineer', 150000, 1),
+('Account Manager', 160000, 2),
+('Accountant' 1250000, 2),
+('Legal Team Lead', 250000, 3),
+('Lawyer', 190000, 3),
+('Sales Person', 80000, 4),
+('Sales Lead', 100000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('John', 'Doe', 4, NULL),
+('Mike', 'Chan', 4, 1),
+('Ashley', 'Rodriguez', 1, NUll),
+('Kevin', 'Tupik', 1, 3),
+('Kunal', 'Singh', 4, NUll),
+('Malia', 'Brown', 2, 5),
+('Sarah', 'Lourd', 3, NUll),
+('Tom', 'Allen', 3, 7);
